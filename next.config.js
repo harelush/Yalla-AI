a91cd5bbcae8ac3d3a3a47dcc238b5623dde0d54
harel.chen <harel.chen@signalpet.com>
2025-08-09 21:27:37 +0300

    Remove basePath and assetPrefix configuration from next.config.js for local development.

--- next.config.js
+++ next.config.js
@@ -4,8 +4,8 @@ const nextConfig = {
   swcMinify: true,
   output: 'export',
   trailingSlash: true,
-  basePath: process.env.NODE_ENV === 'production' ? '/Yalla-AI' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/Yalla-AI/' : '',
+  // basePath: process.env.NODE_ENV === 'production' ? '/Yalla-AI' : '',
+  // assetPrefix: process.env.NODE_ENV === 'production' ? '/Yalla-AI/' : '',
   images: {
     unoptimized: true
   }
